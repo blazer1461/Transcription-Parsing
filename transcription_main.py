e00920da4452366bcbb0a13f9b13b90fafc24594
Spencer Ha <spencer.t.ha@gmail.com>
2021-02-10 17:51:12 -0500

    first draft of csv file. Note that you must change the path and write path on lines 15 and 19 respectively to the location of the .cha file and the folder where you want the css field to go.

--- transcription_main.py
+++ transcription_main.py
@@ -5,40 +5,56 @@
 # Date: 2/9/21
 # Made by: Spencer Ha
 
+import csv
 import os
 from string import digits
 
 #goes through the entire directory and takes all cha files. change the listdir
 #to your dir with the cha files
 def main():
-    path = "in_conversation_corpus_transcripts/"
+    path = "/Users/spencer/Desktop/Transcription-Parsing/cha_files/"
     filelist = os.listdir(path)
 
-    remove_digits = str.maketrans('', '', digits)
-    writer = open('transcribed_file.txt', 'w') 
+    #change the path to the folder directory where you want your csv files to go to
+    write_path = "/Users/spencer/Desktop/Transcription-Parsing/csv_files"
+
+    writer_txt = open('transcribed_file.txt', 'w')
     for i in filelist:
+        writer_csv = open(os.path.join(write_path, i + '.csv'), 'w')
+        csv_writer = csv.writer(writer_csv, delimiter=',')
         if i.endswith(".cha"):
             with open(path + i, 'r') as file:
                 print(path + i)
                 # if you want a header line saying which file is being transcribed  uncomment the next line
-                # writer.write("**** File being transcribed is " + str(i) +" ****" + "\n")
+                # writer_txt.write("**** File being transcribed is " + str(i) +" ****" + "\n")
                 for line in file:
                     if line.startswith('@') or line.startswith('\t'):
                         continue
-                #removes all characters after the '%'. Can change if needed
+                    #probably could make the next 3 lines more modualar, but need it to creater the speaker_ID variable before deletion
+                    #removes all characters after the '%'. Can change if needed
                     sep = '%'
-                    line = line.split(sep, -1)[0] 
+                    line = line.split(sep, -1)[0]
                     line = line.translate({ord(z): None for z in '⌈⌋⌊⌉_-_().*≈><\t?,∆°'})
-                    line = line.translate(remove_digits)
-                    line = line.replace("SP:", "")
-                    line = line.strip()
-                    # removes all whitespace around the line (important to do this BEFORE adding the \n, after removing overlaps)                    
-                    line = line + "\n"
-                #removes all ⌈⌋⌊⌉().*
-                    line = line.replace(":", "")
-                    line = line.replace("  ", " ")
-                    line = line.replace("uhm", "um")
+                    if (line[0:2] == 'SP'):
+                        speaker_ID = line[0:3]
+                    #Deletes all remaining special characters and digits
+                    line = line_manipulation(line)
                     print(line)
-                    writer.writelines(line)
+                    writer_txt.writelines(line)
+                    csv_writer.writerow([speaker_ID,line])
+
+def line_manipulation(line):
+    remove_digits = str.maketrans('', '', digits)
+
+    line = line.translate(remove_digits)
+    line = line.replace("SP:", "")
+    line = line.strip()
+    # removes all whitespace around the line (important to do this BEFORE adding the \n, after removing overlaps)                    
+    line = line + "\n"
+    #removes all ⌈⌋⌊⌉().*
+    line = line.replace(":", "")
+    line = line.replace("  ", " ")
+    line = line.replace("uhm", "um")
+    return line
 
 main() 
