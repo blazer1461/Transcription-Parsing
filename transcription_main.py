fc033ba80ff7478949f0ee45d1b0dfc9a6878894
theRealJuliaMertens <42388620+theRealJuliaMertens@users.noreply.github.com>
2021-02-10 09:51:21 -0500

    Update transcription_main.py
    Additional features: 
    - replace "uhm" with "um" 
    - delete ?,∆°_-_
    - trip whitespace from the lines, after removing overlaps
    - reduce the number of blank lines 
    
    Interesting issues: 
    - there are still some extra blank lines. Should be okay to use with these lines, but I'm not sure why these lines are being created
    
    Notes: 
    - although sep = "%", the old bullet symbol, the code seems to also work on the new bullet symbol. This is a bit of a mystery to me

--- transcription_main.py
+++ transcription_main.py
@@ -11,33 +11,34 @@ from string import digits
 #goes through the entire directory and takes all cha files. change the listdir
 #to your dir with the cha files
 def main():
-    path = "/Users/spencer/Desktop/Transcription-Parsing/cha_files/"
+    path = "in_conversation_corpus_transcripts/"
     filelist = os.listdir(path)
 
     remove_digits = str.maketrans('', '', digits)
-    writer= open('transcribed_file.txt', 'w') 
+    writer = open('transcribed_file.txt', 'w') 
     for i in filelist:
         if i.endswith(".cha"):
             with open(path + i, 'r') as file:
+                print(path + i)
                 # if you want a header line saying which file is being transcribed  uncomment the next line
                 # writer.write("**** File being transcribed is " + str(i) +" ****" + "\n")
                 for line in file:
-                    if line.startswith('@'):
+                    if line.startswith('@') or line.startswith('\t'):
                         continue
                 #removes all characters after the '%'. Can change if needed
                     sep = '%'
-                    line = line.split(sep, -1)[0] + '\n'
-                #removes all ⌈⌋⌊⌉().*
-                    line = line.translate({ord(z): None for z in '⌈⌋⌊⌉().*≈><\t'})
+                    line = line.split(sep, -1)[0] 
+                    line = line.translate({ord(z): None for z in '⌈⌋⌊⌉_-_().*≈><\t?,∆°'})
                     line = line.translate(remove_digits)
                     line = line.replace("SP:", "")
+                    line = line.strip()
+                    # removes all whitespace around the line (important to do this BEFORE adding the \n, after removing overlaps)                    
+                    line = line + "\n"
+                #removes all ⌈⌋⌊⌉().*
+                    line = line.replace(":", "")
+                    line = line.replace("  ", " ")
+                    line = line.replace("uhm", "um")
+                    print(line)
                     writer.writelines(line)
-                writer.write("\n")
 
-main()
-
-            
-            
-
-
-
+main() 
