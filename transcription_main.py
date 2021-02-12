cb09befdc1cb7be4cfeb1dcc691aa9ecfccac076
theRealJuliaMertens <42388620+theRealJuliaMertens@users.noreply.github.com>
2021-02-12 13:52:27 -0500

    Update transcription_main.py
    Added a few more symbols, and fixed the blank line problem by making the "if" statement only process lines that start with "*". However, this means it's extremely important that the turn is all on the same line, no matter how long it is.

--- transcription_main.py
+++ transcription_main.py
@@ -1,7 +1,7 @@
 # First Draft Transciption.
-# transciption_main.py
+# export to .csv
 # Purpose: Takes all .cha files from a directory and parse through the files
-#          which has special characters. Output them all to a seperate new text #          file
+#          which has special characters. Output them all to a seperate new csv ile
 # Date: 2/9/21
 # Made by: Spencer Ha
 
@@ -12,11 +12,11 @@ from string import digits
 #goes through the entire directory and takes all cha files. change the listdir
 #to your dir with the cha files
 def main():
-    path = "/Users/spencer/Desktop/Transcription-Parsing/cha_files/"
+    path = "In_conversation_corpus_transcripts/"
     filelist = os.listdir(path)
 
     #change the path to the folder directory where you want your csv files to go to
-    write_path = "/Users/spencer/Desktop/Transcription-Parsing/csv_files"
+    write_path = "csv_files"
 
     writer_txt = open('transcribed_file.txt', 'w')
     for i in filelist:
@@ -29,21 +29,23 @@ def main():
                 # if you want a header line saying which file is being transcribed  uncomment the next line
                 # writer_txt.write("**** File being transcribed is " + str(i) +" ****" + "\n")
                 for line in file:
-                    if line.startswith('@') or line.startswith('\t'):
-                        continue
+                #    if line.startswith('@') or line.startswith('\t') or line.startswith('%'):
+                #        continue
                     #probably could make the next 3 lines more modualar, but need it to creater the speaker_ID variable before deletion
                     #removes all characters after the '%'. Can change if needed
-                    sep = '%'
-                    line = line.split(sep, -1)[0]
-                    line = line.translate({ord(z): None for z in '⌈⌋⌊⌉_-_().*≈><\t?,∆°'})
-                    if (line[0:2] == 'SP'):
-                        speaker_ID = line[0:3]
-                    #Deletes all remaining special characters and digits
-                    line = line_manipulation(line)
-                    print(line)
-                    writer_txt.writelines(line)
-                    counter = counter + 1
-                    csv_writer.writerow([counter,speaker_ID,line])
+                    if line[0] == '*':
+                    
+                        sep = '%'
+                        line = line.split(sep, -1)[0]
+                        line = line.translate({ord(z): None for z in '⌈⌋⌊⌉_-_().*≈><\t?,∆°↗⇘∬↘⁇↑Ã≤='})
+                        if (line[0:2] == 'SP'):
+                            speaker_ID = line[0:3]
+                        #Deletes all remaining special characters and digits
+                        line = line_manipulation(line)
+                        print(line)
+                        writer_txt.writelines(line)
+                        counter = counter + 1
+                        csv_writer.writerow([counter,speaker_ID,line])
 
 def line_manipulation(line):
     remove_digits = str.maketrans('', '', digits)
@@ -56,6 +58,7 @@ def line_manipulation(line):
     #removes all ⌈⌋⌊⌉().*
     line = line.replace(":", "")
     line = line.replace("  ", " ")
+    line = line.replace("Mhmm", "mhm")
     line = line.replace("uhm", "um")
     return line
 
